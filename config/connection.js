c61b5c607c41ee11812574dd428f5466c8cccf1e
goofy foot <williamk31@gmail.com>
2024-04-05 14:56:39 -0600

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
+const sequelize =  new Sequelize(
     process.env.DB_NAME,
-    process.env.DB_PASSWORD,
     process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
-      dialect: 'postgres'
+      dialect: 'postgres',
     }
   );
-}
 
-module.exports = sequelize
+module.exports = sequelize;
